ca1432fa46d11bcb19c6cd81ff95026d6194dd84
Aleksey <kravchenko.a.a.rb@gmail.com>
2022-07-27 19:53:42 +0300

    add weather for city

--- lib/screens/weather_forecast_widget.dart
+++ lib/screens/weather_forecast_widget.dart
@@ -6,6 +6,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:weather/api/weather_api.dart';
 import 'package:weather/models/weather_forecast_daily.dart';
+import 'package:weather/screens/city_screen.dart';
 import 'package:weather/widgets/bottom_list_view.dart';
 import 'package:weather/widgets/city_view.dart';
 import 'package:weather/widgets/detail_view.dart';
@@ -21,16 +22,12 @@ class WeatherForecastScreen extends StatefulWidget {
 class _WeatherForecastScreenState extends State<WeatherForecastScreen> {
   Future<WeatherForecast>? forecastObject;
   String _cityName = 'London';
-
+  // String? _cityName;
   @override
   void initState() {
-    super.initState();
     forecastObject =
         WeatherApi().fetchWeatherForecastWithCity(cityName: _cityName);
-
-    // forecastObject?.then((weather) {
-    //   print(weather.list?[0].weather?[0].main);
-    // });
+    super.initState();
   }
 
   @override
@@ -46,8 +43,20 @@ class _WeatherForecastScreenState extends State<WeatherForecastScreen> {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
             icon: Icon(Icons.location_city),
+            onPressed: () async {
+              var tappedName = await Navigator.push(context,
+                  MaterialPageRoute(builder: (context) {
+                return CityScreen();
+              }));
+              if (tappedName != null) {
+                setState(() {
+                  _cityName = tappedName;
+                  forecastObject = WeatherApi()
+                      .fetchWeatherForecastWithCity(cityName: _cityName);
+                });
+              }
+            },
           ),
         ],
       ),
@@ -57,7 +66,7 @@ class _WeatherForecastScreenState extends State<WeatherForecastScreen> {
             child: FutureBuilder<WeatherForecast>(
               future: forecastObject,
               builder: (context, snapshot) {
-                if(snapshot.hasData) {
+                if (snapshot.hasData) {
                   return Column(
                     children: [
                       SizedBox(height: 50),
